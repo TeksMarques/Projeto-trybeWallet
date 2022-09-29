1abac0ab03a98c064ef53a5d44908c46c353c285
Teresa Marques <taotaodoce@gmail.com>
2022-09-29 18:04:47 -0300

    requisito 1 concluído

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getUser } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleClick = (email) => {
+    const { getUserEmail, history } = this.props;
+    getUserEmail(email);
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    const usuario = /\S+[@]\w+[.]\w+/gm;
+    const min = 6;
+    const condition = (usuario.test(email) && password.length >= min);
+    return (
+      <div>
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ ({ target: { value } }) => this.setState({ email: value }) }
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value } }) => this.setState({ password: value }) }
+        />
+
+        <button
+          type="button"
+          disabled={ !condition }
+          onClick={ () => this.handleClick(email) }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  getUserEmail: (email) => dispatch(getUser(email)),
+});
+
+Login.propTypes = {
+  getUserEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const GET_USER = 'GET_USER';
+
+export const getUser = (email) => ({
+  type: GET_USER,
+  email,
+});
