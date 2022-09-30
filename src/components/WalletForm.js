bea942d00ec7809054e584330ea8b4d8c1d6e126
Teresa Marques <taotaodoce@gmail.com>
2022-09-29 21:05:27 -0300

    tentanto resolver requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 class WalletForm extends Component {
+  state = {
+    moedas: [],
+  };
+
+  componentDidMount() {
+    this.handleApi();
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleApi = async () => {
+    const api = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const result = await api.json();
+    const coin = Object.keys(result);
+    const filtro = coin.filter((moeda) => moeda !== 'USDT');
+    this.setState({
+      moedas: filtro,
+    });
+  };
+
   render() {
     return (
-      <div>WalletForm</div>
+      <form>
+        <div>
+          <input
+            type="number"
+            data-testid="value-input"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            data-testid="description-input"
+          />
+        </div>
+        <div>
+          <select name="moedas" data-testid="currency-input">
+            {
+              moedas.map((moeda) => (
+                <option value={ moeda } key="moeda">{moeda}</option>
+              ))
+            }
+          </select>
+        </div>
+        <div>
+          <select name="pagamento" data-testid="method-input">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="cartao-de-credito">Cartão de crédito</option>
+            <option value="cartao-de-debito">Cartão de débito</option>
+          </select>
+        </div>
+        <div>
+          <select name="tipo" data-testid="tag-input">
+            <option value="alimentacao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saude">Saúde</option>
+          </select>
+        </div>
+      </form>
     );
   }
 }
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { GET_WALLET } from '../actions/index';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expensies: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case GET_WALLET:
+    return { ...state, currencies: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
